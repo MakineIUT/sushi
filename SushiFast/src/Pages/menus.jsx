420ae628b3dddd33f544cd1f524ffe21598dc137
Goncalves Hugo <fernandes93.hg@gmail.com>
2025-12-10 15:05:22 +0100

    ajoute de tout les filtres

--- SushiFast/src/Pages/menus.jsx
+++ SushiFast/src/Pages/menus.jsx
@@ -1,24 +1,35 @@
 import { useState, useEffect } from "react";
-import boxes from "../DATA/boxes.json";
+import DATAboxes from "../DATA/boxes.json";
 
 function Menus() {  
   const [Boxes, setBoxes] = useState([]);
 
 useEffect(() => {
-  setBoxes(boxes);
+  setBoxes(DATAboxes);
 }, []);
 
-const F_saveurs = (saveurs) => {
-  const filtered = boxes.filter(box => box.saveurs.includes(saveurs));
+const F_saveurs = (saveur) => {
+  const filtered = DATAboxes.filter(box => box.saveurs.includes(saveur));
   setBoxes(filtered);
 };
 
-const F = (nom) => {
-  const filtered = boxes.filter(box => box.nom.includes(nom));
-  setBoxes(filtered);
-};
+const F = (nomAliment) => {
+    const term = nomAliment.toLowerCase();
+    
+    setBoxes(boxes.filter(box => 
+      box.aliments.some(aliment => aliment.nom.toLowerCase().includes(term))
+    ));
+  };
   
-  
+const F_Croissant = () => {
+    const trie = [...Boxes].sort((a, b) => a.prix - b.prix);
+    setBoxes(trie);
+  };
+
+const F_Decroissant = () => {
+    const trie = [...Boxes].sort((a, b) => b.prix - a.prix);
+    setBoxes(trie);
+  };
 
   return (
     <div className="max-w-7xl mx-auto p-8">
@@ -27,8 +38,13 @@ const F = (nom) => {
         <button className="bg-red-500 mr-4 rounded-full  hover:bg-red-600 transition px-6 py-2 text-white" onClick={() => setBoxes(boxes)}>Tous</button>
         <button className="bg-red-500 mr-4 rounded-full  hover:bg-red-600 transition px-6 py-2 text-white" onClick={() => F_saveurs("avocat")}>Avocat</button>
        <button className="bg-red-500 mr-4 rounded-full  hover:bg-red-600 transition px-6 py-2 text-white" onClick={() => F_saveurs("coriandre")}>Coriandre</button>
-
         <button className="bg-red-500 mr-4 rounded-full  hover:bg-red-600 transition px-6 py-2 text-white" onClick={() => F("California Saumon Avocat")}>California Saumon Avocat</button>
+        <button className="bg-blue-600 mr-4 rounded-full hover:bg-blue-700 transition px-6 py-2 text-white" onClick={() => F_Croissant()}>
+          Prix Croissant ↑
+        </button>
+        <button className="bg-blue-600 mr-4 rounded-full hover:bg-blue-700 transition px-6 py-2 text-white" onClick={() => F_Decroissant()}>
+          Prix Décroissant ↓
+        </button>
      
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
